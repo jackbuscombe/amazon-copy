4cab9fc4d1f002693d51b24bf83a3898553f38e8
Jack Buscombe <jackbuscombe94@gmail.com>
2022-04-18 16:22:47 +1000

    Home Page Front-End Build

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				amazon_blue: {
+					light: "#232F3E",
+					DEFAULT: "#131921",
+				},
+			},
+		},
+	},
+	plugins: [require("@tailwindcss/line-clamp")],
+	darkMode: false,
+};
